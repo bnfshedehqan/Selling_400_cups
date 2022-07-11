4abbcac50c750ffacdaa612aaaaa7557d93b717a
Banafshe Dehqan <64371848+bnfshedehqan@users.noreply.github.com>
2022-07-11 22:31:41 +0430

    Selling 400 cups

---
+++ Selling_400_cups.py
@@ -0,0 +1,6 @@
+# قیمت یک لیوان 5200 است
+# تعداد خریداری شده 400 عدد است 
+The_price_of_a_glass=5200
+The_number_of_glasses=400
+total_price=The_price_of_a_glass*The_number_of_glasses
+print(total_price)
